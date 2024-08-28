5e9ac27a9f9aa23c7dd055d93bf178844ee2b411
vicsaa01 <victorsaakes@gmail.com>
2024-08-28 15:15:07 +0200

    fix #1

--- site/src/index.js
+++ site/src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
